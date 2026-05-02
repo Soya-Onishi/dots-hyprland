a26b0b23b59c385fa0ab92842bd0653c8aaac996
Soya Ohnishi <Soya Ohnishi>
2026-05-02 20:14:17 +0900

    fix to exec setup on launch

--- dots/.config/hypr/custom/scripts/monitor-watch.sh
+++ dots/.config/hypr/custom/scripts/monitor-watch.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SETUP_SCRIPT="~/.config/hypr/custom/scripts/monitor-setup.sh"
+
 # 自分自身の古いプロセスを終了（現在のPIDは除く）
 # pkillが完了するのを少し待つ
 # 自プロセスグループ以外の古いインスタンスを終了
@@ -12,10 +14,11 @@ sleep 1.5
 
 handle() {
     case $1 in
-        monitoradded*)   ~/.config/hypr/scripts/monitor-setup.sh ;;
-        monitorremoved*) ~/.config/hypr/scripts/monitor-setup.sh ;;
+        monitoradded*)   $SETUP_SCRIPT ;;
+        monitorremoved*) $SETUP_SCRIPT ;;
     esac
 }
 
+$SETUP_SCRIPT
 socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" \
     | while read -r line; do handle "$line"; done
