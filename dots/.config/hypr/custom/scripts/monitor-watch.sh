0ce3feeb041347dde1c4f9ead79cf813e0108609
Soya Ohnishi <Soya Ohnishi>
2026-05-02 20:11:14 +0900

    fix script and exec config

--- dots/.config/hypr/custom/scripts/monitor-watch.sh
+++ dots/.config/hypr/custom/scripts/monitor-watch.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# 自分自身の古いプロセスを終了（現在のPIDは除く）
+# pkillが完了するのを少し待つ
+# 自プロセスグループ以外の古いインスタンスを終了
+for pid in $(pgrep -f "/.config/hypr/custom/scripts/monitor-watch.sh"); do
+    if [ "$(ps -o pgid= -p $pid | tr -d ' ')" != "$(ps -o pgid= -p $$ | tr -d ' ')" ]; then
+        kill "$pid"
+    fi
+done
+sleep 1.5
+
 handle() {
     case $1 in
         monitoradded*)   ~/.config/hypr/scripts/monitor-setup.sh ;;
